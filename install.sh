8476b027c019e44fa079a83b955a42745b7e2762
Raita Watanabe <watanaberit20171129@gmail.com>
2020-04-26 01:25:37 +0900

    fix

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -eu
 readonly THIS_SCRIPT_DIR=$(cd "$(dirname $0)"; pwd)
 readonly DOTFILES_AT_THIS_SCRIPT_DIR="${THIS_SCRIPT_DIR}/dotfiles"
 readonly DOTFILES_AT_HOME_DIR="${HOME}/dotfiles"
+readonly DOTFILES_BK_AT_HOME_DIR="${DOTFILES_AT_HOME_DIR}/backup"
 readonly BASHRC="${HOME}/.bashrc"
 
 
@@ -11,7 +12,7 @@ function backup() {
     local target="$1"
 
     local bk
-    bk="${target}.bk.$(date +%Y%m%d%H%M%S)"
+    bk="${DOTFILES_BK_AT_HOME_DIR}/$(basename "${target}").bk.$(date +%Y%m%d%H%M%S)"
 
     if [ -e "${target}" ]; then
         cp -r "${target}" "${bk}"
@@ -20,9 +21,12 @@ function backup() {
 
 function install() {
     mkdir -p "${DOTFILES_AT_HOME_DIR}"
-    rsync -ahv "${DOTFILES_AT_THIS_SCRIPT_DIR}/*" "${DOTFILES_AT_HOME_DIR}/"
+    rsync -ahv "${DOTFILES_AT_THIS_SCRIPT_DIR}/" "${DOTFILES_AT_HOME_DIR}/"
 
-    for dot_file in $(find "${DOTFILES_AT_HOME_DIR}" -maxdepth 1 -not -name "${DOTFILES_AT_HOME_DIR}"); do
+    for dot_file in $(find "${DOTFILES_AT_HOME_DIR}" -maxdepth 1); do
+        if [ "${dot_file}" = "${DOTFILES_AT_HOME_DIR}" ]; then
+            continue
+        fi
         backup "${dot_file}"
 
         symlink="${HOME}/$(basename ${dot_file})"
